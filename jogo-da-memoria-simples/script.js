d7c46159cca05cb27fd8510a45386a224c52e35b
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-13 14:56:41 -0300

    implementa funcionalidades de identificar cartas clicadas  como primeira ou segunda carta, desabilitar cartas que combinam, desvirar cartas que não combinam,

--- jogo-da-memoria-simples/script.js
+++ jogo-da-memoria-simples/script.js
@@ -1,7 +1,39 @@
 const cartas = document.querySelectorAll('.carta');
+let cartaVirada = false;
+let primeiraCartaVirada, segundaCartaVirada; 
+
 
 function virarCarta() {
-  this.classList.toggle('virar');
+  this.classList.add('virar');
+  if(!cartaVirada){
+    cartaVirada = true;
+    primeiraCartaVirada = this;
+    return;
+  }
+
+  segundaCartaVirada = this;
+  cartaVirada = false;
+  checarSeCombina();
+}
+
+function checarSeCombina() {
+  if(primeiraCartaVirada.dataset.card === segundaCartaVirada.dataset.card) {
+    desabilitarCartas();
+    return;
+  }
+  desvirarCartas();
+}
+
+function desabilitarCartas() {
+  primeiraCartaVirada.removeEventListener('click', virarCarta);
+  segundaCartaVirada.removeEventListener('click', virarCarta);
+}
+
+function desvirarCartas() {
+  setTimeout(() => {
+    primeiraCartaVirada.classList.remove('virar');
+    segundaCartaVirada.classList.remove('virar');
+  }, 1000);
 }
 
 cartas.forEach( carta => carta.addEventListener('click', virarCarta))
