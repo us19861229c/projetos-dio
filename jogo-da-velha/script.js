3360c011ba44e006d41312945ea501639a2957c3
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-15 13:17:57 -0300

    cria dinâmica do jogo ainda sem checagem de combinação.

---
+++ jogo-da-velha/script.js
@@ -0,0 +1,48 @@
+let jogador = null;
+let jogadorAtual = document.querySelector('#jogador-selecionado') ;
+
+jogadorXouO('X')
+
+function jogadorXouO(valor) {
+  jogador = valor
+  jogadorAtual.innerHTML = jogador; 
+}
+
+const quadrado1 = document.querySelector('#q1');
+const quadrado2 = document.querySelector('#q2');
+const quadrado3 = document.querySelector('#q3');
+const quadrado4 = document.querySelector('#q4');
+const quadrado5 = document.querySelector('#q5');
+const quadrado6 = document.querySelector('#q6');
+const quadrado7 = document.querySelector('#q7');
+const quadrado8 = document.querySelector('#q8');
+const quadrado9 = document.querySelector('#q9');
+
+function escolherQuadrado(id) {
+  let quadrado = document.getElementById(id);
+  if (quadrado.innerHTML !== '-') {
+    alert('esse quadrado já foi escolhido!')
+    return;
+  }
+
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = '#000';
+
+  if(jogador === 'X') {
+    jogador = "O";
+  } else {
+    jogador = "X";
+  }
+
+  jogadorXouO(jogador);
+}
+
+quadrado1.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado2.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado3.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado4.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado5.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado6.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado7.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado8.onclick = (event) => escolherQuadrado(event.srcElement.id);
+quadrado9.onclick = (event) => escolherQuadrado(event.srcElement.id);
