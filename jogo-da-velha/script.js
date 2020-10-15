df3c26964e7fdd2252d074f42804566b40c58a75
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-15 14:08:51 -0300

    cria a análise de combinações, destaca com css a combinação vencedora e revela o vencedor no html

--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -1,6 +1,8 @@
-let jogador = null;
-let jogadorAtual = document.querySelector('#jogador-selecionado') ;
-const botaoReiniciarJogo = document.querySelector('#reiniciar')
+let jogador, jogadorVencedor = null;
+let jogadorAtual = document.querySelector('#jogador-selecionado');
+let vencedorAtual = document.querySelector('#vencedor')
+const botaoReiniciarJogo = document.querySelector('#reiniciar');
+
 
 jogadorXouO('X')
 
@@ -36,16 +38,87 @@ function escolherQuadrado(id) {
   }
 
   jogadorXouO(jogador);
+  quemVenceu();
+}
+
+function quemVenceu() {
+  if (checarSeCombina(quadrado1, quadrado2, quadrado3)) {
+    destacaCombinados(quadrado1, quadrado2, quadrado3);
+    destacaVencedor(quadrado1);
+    return;
+  }
+
+  if (checarSeCombina(quadrado4, quadrado5, quadrado6)) {
+      destacaCombinados(quadrado4, quadrado5, quadrado6);
+      destacaVencedor(quadrado4);
+      return;
+  }
+
+  if (checarSeCombina(quadrado7, quadrado8, quadrado9)) {
+      destacaCombinados(quadrado7, quadrado8, quadrado9);
+      destacaVencedor(quadrado7);
+      return;
+  }
+
+  if (checarSeCombina(quadrado1, quadrado4, quadrado7)) {
+      destacaCombinados(quadrado1, quadrado4, quadrado7);
+      destacaVencedor(quadrado1);
+      return;
+  }
+
+  if (checarSeCombina(quadrado2, quadrado5, quadrado8)) {
+      destacaCombinados(quadrado2, quadrado5, quadrado8);
+      destacaVencedor(quadrado2);
+      return;
+  }
+
+  if (checarSeCombina(quadrado3, quadrado6, quadrado9)) {
+      destacaCombinados(quadrado3, quadrado6, quadrado9);
+      destacaVencedor(quadrado3);
+      return;
+  }
+
+  if (checarSeCombina(quadrado1, quadrado5, quadrado9)) {
+      destacaCombinados(quadrado1, quadrado5, quadrado9);
+      destacaVencedor(quadrado1);
+      return;
+  }
+
+  if (checarSeCombina(quadrado3, quadrado5, quadrado7)) {
+      destacaCombinados(quadrado3, quadrado5, quadrado7);
+      destacaVencedor(quadrado3);
+      return;
+  }
+}
+
+function checarSeCombina( quadradoA, quadradoB, quadradoC) {
+  let combinamEntreSi = false;
+  if (quadradoA.innerHTML !== '-' && quadradoA.innerHTML === quadradoB.innerHTML && quadradoB.innerHTML === quadradoC.innerHTML) {
+    combinamEntreSi = true;
+  }
+  return combinamEntreSi;
+}
+
+function destacaVencedor(quadrado) {
+  jogadorVencedor = quadrado.innerHTML;
+  vencedorAtual.innerHTML = jogadorVencedor;
+}
+
+function destacaCombinados(quadradoA, quadradoB, quadradoC) {
+  quadradoA.style.background = '#0f0';
+  quadradoB.style.background = '#0f0';
+  quadradoC.style.background = '#0f0';
 }
 
 function reiniciaJogo() {
-
+ 
   for (let pos = 1; pos <= 9; pos += 1) {
       let quadrado = document.querySelector(`#q${pos}`);
       quadrado.style.background = '#eee';
       quadrado.style.color = '#eee';
       quadrado.innerHTML = '-';
   }
+  vencedorAtual.innerHTML = '';
   jogadorXouO('X');
 }
 
